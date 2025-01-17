05d428579ee0d48d3c8abe8a725acba91f13aeba
devkev00 <kimminsoo116@gmail.com>
2025-01-18 01:06:57 +0900

    feat: form 활용하여 입력값 체크하기, 타임리프 활용해서 form 연동 오류 메시지 띄우기, th:field 속성 이용하여 기존 입력값 유지하기

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,22 +2,32 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/answer")
-@RestController // @RequestBody + @Controller
+@Controller //
 public class AnswerController {
 
     private final QuestionService questionService;
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(@PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
 
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            // question_detail 템플릿은 Question 객체가 필요함
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 
