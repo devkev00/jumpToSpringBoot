3455e8ebbeb5483be8c44915e8eae7a96d1bdfcc
devkev00 <kimminsoo116@gmail.com>
2025-01-17 23:36:29 +0900

    feat: 답변 create 기능, 답변 list 표시

---
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+@RestController // @RequestBody + @Controller
+public class AnswerController {
+
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(@PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question, content);
+        return String.format("redirect:/question/detail/%s", id);
+    }
+
+}
