9b32783a5c4c3105f2264146a85c3c79b35fc975
devkev00 <kimminsoo116@gmail.com>
2025-01-19 23:05:27 +0900

    feat: 스프링 시큐리티 설정

---
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 활성화
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()) // 미인증된 모든 페이지 요청 허락
+        ;
+        return http.build();
+    }
+}
