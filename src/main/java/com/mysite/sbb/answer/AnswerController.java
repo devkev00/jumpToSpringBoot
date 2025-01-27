fecf21dcc2d355376f5b6e7933c93f658a6ec64d
devkev00 <kimminsoo116@gmail.com>
2025-01-28 00:41:07 +0900

    feat: 엔티티 작성자 추가, 로그인 여부 검증

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,12 +2,17 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @RequestMapping("/answer")
@@ -16,18 +21,23 @@ public class AnswerController {
 
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final UserService userService;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id,
+                               @Valid AnswerForm answerForm,
+                               BindingResult bindingResult, Principal principal) {
 
         Question question = this.questionService.getQuestion(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
 
         if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
             // question_detail 템플릿은 Question 객체가 필요함
             return "question_detail";
         }
-        this.answerService.create(question, answerForm.getContent());
+        this.answerService.create(question, answerForm.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s", id);
     }
 
